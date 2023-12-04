d86bf66c30d492c0b3776d564ea6784a402ec073
Pawel Franczak <pawel.franczak@dbschenker.com>
2023-12-04 12:32:23 +0100

    add script to check kube manifest for default ns

---
+++ jenkins-inbound-agent/bin/check-default-namespace.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+## Check if the kubernetes manifest have explicitly set default namespace
+if [ "$#" -eq 0 ]
+then
+  echo "Path to Kubernetes yaml manifests is required"
+  exit 1
+else
+    cat $1 | grep -E '^kind:|^\s{2}name:|^\s{2}namespace:\s{0,}"{0,1}default"{0,1}\s{0,}$' | grep -E 'namespace:' -B2
+fi
